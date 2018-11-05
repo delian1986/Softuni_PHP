09a6ca24145fe8fa2c7e3ed59955fb6a30d85bad
delian1986 <delian1914@gmail.com>
2018-11-05 21:18:07 +0200

    common option to pdo array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION) added

--- PHPTasks/Skeleton/common.php
+++ PHPTasks/Skeleton/common.php
@@ -4,7 +4,7 @@ spl_autoload_register();
 
 $template= new \Core\Template();
 $dbInfo=parse_ini_file('Config/db.ini');
-$pdo=new PDO($dbInfo['dsn'],$dbInfo['user'],$dbInfo['password']);
+$pdo=new PDO($dbInfo['dsn'],$dbInfo['user'],$dbInfo['password'],array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 $db= new Database\PDODatabase($pdo);
 $userRepository=new \App\Repository\UserRepository($db);
 $userService=new \App\Service\UserService($userRepository);
